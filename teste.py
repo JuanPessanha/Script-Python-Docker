b5fb8ee4d113086566f2eed8f89e76608e433b50
JuanPessanha <juanpessanha@gmail.com>
2017-11-27 15:47:21 -0200

    usando o cmd para realizar as requisições

--- teste.py
+++ teste.py
@@ -12,4 +12,30 @@ if 'status' in comando[0]:
         print(client.containers.get(comando[1]))
     else:
         print('container ID not found\n')
+        
+        
+# usando o cmd para realizar as requisições
+import subprocess
+loop = 1
+proc = subprocess.Popen(['hostname'], stdout=subprocess.PIPE, shell=True)
+(out, err) = proc.communicate()
+containers = str(out)
+print(containers)
+
+while loop != 0:
+    comando = input('>>> ').lower().strip().split()
+
+#checar como conseguir todo o container id
+    if 'status' in comando:
+        subprocess.call(comando[1])
+        print('\n')
+
+    elif comando == ['amoeba']:
+        print('Olha no que deu\n')
+
+    elif comando == ['exit']:
+        exit()
+
+    else:
+        print('por favor digite um comando válido\n')
 
