cfdada4111892d8af5c3c04c257c56322e123746
Juan <juanpessanha@gmail.com>
2017-11-27 23:41:57 -0200

    adicionados os comandos start, stop, exec e status.

--- Sript python-docker.py
+++ Sript python-docker.py
@@ -1,6 +1,5 @@
-import docker
-client = docker.from_env()
-containers1 = str(client.containers.list(all)).split()
+#import docker
+from subprocess import call, Popen
 loop = 1
 
 print('-=-' * 20)
@@ -10,18 +9,29 @@ print('-=-' * 20, '\n')
 while loop != 0:
     comando = input('>>> ').lower().strip().split()
 
-#checar como conseguir todo o container id
+#checar como exibir apenas o nome, imagem e status (execução)
     if 'status' in comando:
-        if comando[1] + '>,' in containers1 or comando[1] + '>]' in containers1:
-            print(client.containers.get(comando[1]))
-        else:
-            print('container ID not found\n')
+        call('docker ps -a --filter id={}'.format(comando[1]), shell=False,)
+        print('\n')
 
-    elif comando == ['amoeba']:
-        print('Olha no que deu\n')
+#adcionar o tempo
+    elif 'start' in comando:
+        p = call('docker start {}'.format(comando[1]))
+        print('Container {} iniciado'.format(comando[1]))
 
+#Comando que para a execução do container
+    elif 'stop' in comando:
+        Popen('docker stop {}'.format(comando[1]), shell=False)
+        print('Container {} parado'.format(comando[1]))
+
+#comando que abre o cmd dentro de um container para execução de comandos
+    elif 'exec' in comando:
+        p = call('docker exec -it {} cmd'.format(comando[1]), shell=True)
+
+#Sair do Script
     elif comando == ['exit']:
         exit()
 
+#comando desconhecido
     else:
         print('favor digite um comando válido\n')
