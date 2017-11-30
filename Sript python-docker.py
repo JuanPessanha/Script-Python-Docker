3088dfb27d97631f2d9e51dcb9fdd143fc5e7419
Juan <juanpessanha@gmail.com>
2017-11-29 22:51:02 -0200

    definidas funções para os comandos, para facilitar a futuras expansões

--- Sript python-docker.py
+++ Sript python-docker.py
@@ -1,10 +1,10 @@
 from os import devnull
-from subprocess import call, Popen, PIPE, STDOUT
+from subprocess import call, Popen, STDOUT
 
 loop = 1
 
 print('-=-' * 30)
-print('Python Script to Management Docker')
+print('Scrip Python para gerenciamento do Docker')
 print('-=-' * 30, '\n')
 
 #iniciando estrutura de repetição
@@ -12,32 +12,33 @@ while loop != 0:
     comando = input('>>> ').lower().strip().split()
     FNULL = open(devnull, 'w')
 
+#funções de comando
+    def start():
+        if 'start' in comando:
+            if '-t' in comando:
+                call('docker start {}'.format(comando[3]))
+                Popen('ping 127.0.0.1 -n {} > nul && docker stop {}'.format(int(comando[2]) + 1, comando[3]),
+                      shell=True, stdout=FNULL, stderr=STDOUT)
+            else:
+                call('docker start {}'.format(comando[1]))
 
-#checar stats de container (execução)
-    if 'status' in comando:
-        call('docker ps -a --filter id={}'.format(comando[1]), shell=True)
+    def stop():
+        if 'stop' in comando:
+            call('docker stop {}'.format(comando[1]))
 
-#Iniciar a execução de um container
-    elif 'start' in comando:
-        #com o parâmetro para seu stop automático
-        if '-t' in comando:
-            call('docker start {}'.format(comando[3]))
-            Popen('ping 127.0.0.1 -n {} > nul && docker stop {}'.format(int(comando[2]) + 1, comando[3]), shell=True, stdout=FNULL, stderr=STDOUT)
-        else:
-            call('docker start {}'.format(comando[1]))
+    def status():
+        if 'status' in comando:
+            call('docker ps -a --filter id={}'.format(comando[1]), shell=True)
 
-#Comando que para a execução do container
-    elif 'stop' in comando:
-        call('docker stop {}'.format(comando[1]))
+    def executar():
+        if 'exec' in comando:
+            call('docker exec -it {} cmd'.format(comando[1]), shell=True)
 
-#comando que abre o cmd dentro de um container para execução de comandos
-    elif 'exec' in comando:
-        call('docker exec -it {} cmd'.format(comando[1]), shell=True)
+    def fechar():
+        if comando == ['exit']:
+            exit()
 
-#Sair do Script
-    elif comando == ['exit']:
-        exit()
+#input de comando
+    if comando is not None:
+        start(), stop(), status(), executar(), fechar()
 
-#comando desconhecido
-    else:
-        print('favor digite um comando válido\n')
